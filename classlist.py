cf1f16139be9c25f2ca368063cfb4fe43d73dfb6
htlong77 <htlong77@gmail.com>
2020-10-08 09:06:43 +0700

    thử tạo file .tex phiên bản đầu tiên

--- classlist.py
+++ classlist.py
@@ -1,12 +1,18 @@
 import os
-# Create .pdf file from .tex file with xelatex
+# Creating .tex file from within python
+
+texfilename = "lastclasslist.tex"
+fout = open(texfilename, "w")
+fout.close()
+
+# Creating .pdf file from .tex file with xelatex
 cmd = "xelatex classlist.tex"
 failure = os.system(cmd)
 if failure:
     print('Creating "classlist.pdf" failed!!!')
 # Viewing classlist.pdf
 cmd = "evince classlist.pdf &"
-failure = os.system(cmd)
-if failure:
-    print('Viewing "classlist.pdf" failed!!!')
+#failure = os.system(cmd)
+#if failure:
+#    print('Viewing "classlist.pdf" failed!!!')
 
