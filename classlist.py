6d3db181365d240608c20d8749b966f5d189dbce
htlong77 <htlong77@gmail.com>
2020-11-05 12:11:05 +0700

    Sản phẩm trước khi về

--- classlist.py
+++ classlist.py
@@ -1,7 +1,19 @@
 import os
 # Creating .tex file from within python
 import sys
-script, subject, class_code, num_of_credits = sys.argv
+
+def import_student_data(filename):
+    """This function will import student data from a csv file (classlist.dat)"""
+    print(f"Importing student data from file '{filename}'")
+    datafile = open(filename,'r')
+    lines = datafile.readlines()
+    print(lines)
+    for line in lines:
+        print(line)
+    datafile.close()
+    return
+
+script, subject, class_code, num_of_credits, day_of_week, class_hours, class_room = sys.argv
 texfilename = "classlist.tex"
 fout = open(texfilename, "w")
 content = r"""\documentclass[a4paper, 12pt]{article}
@@ -24,19 +36,19 @@ content = r"""\documentclass[a4paper, 12pt]{article}
 \centerline{\textbf{HỌC KỲ 1 2020-2021}}
 
 \noindent
-\begin{minipage}[c]{11cm}
+\begin{minipage}[c]{10cm}
  Học phần: \textbf{%s}\\
- Thứ: 4
+ Thứ: %s
 \end{minipage} 
 \hfill
-\begin{minipage}[c]{4.5cm}
+\begin{minipage}[c]{5.5cm}
  Lớp HP: \textbf{%s}\\
- Tiết: 9-10
+ Tiết: %s
 \end{minipage} 
 \hfill
 \begin{minipage}[c]{4cm}
  Số tín chỉ: \textbf{%s}\\
- Giảng đường: 201-T5
+ Giảng đường: %s
 \end{minipage} 
 \vspace{0.2cm}
 \noindent\begin{tabular}{*{9}{|c}|}
@@ -47,7 +59,7 @@ content = r"""\documentclass[a4paper, 12pt]{article}
   \hline
 \end{tabular}
 \end{document}
-""" % (subject, class_code, num_of_credits)
+""" % (subject, day_of_week, class_code, class_hours, num_of_credits, class_room)
 fout.write(content)
 fout.close()
 
@@ -68,3 +80,6 @@ if failure:
 
 # Thử lần cuối xem còn bị đưa classlist.tex lên không
 # Có vẻ đã giải quyết xong https://stackoverflow.com/questions/3833561/why-doesnt-git-ignore-my-specified-file/3833675#:~:text=gitignore%20files%20don't%20work,matching%20rules%20will%20be%20skipped.
+
+filename = 'classlist.dat'
+import_student_data(filename)
